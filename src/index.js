7211467f56b8d4930c4d1baba0572bb94c9a2b71
Grigory Ptashko <grigory.ptashko@gmail.com>
2017-02-07 11:41:19 +0300

    Resolve Router prop if it is a function. I.e. it adds an ability to switch forceFetch between routes transitions

--- src/index.js
+++ src/index.js
@@ -5,11 +5,21 @@ import RouteContainer from './RouteContainer';
 import getRouteQueries from './utils/getRouteQueries';
 
 export default {
-  renderRouterContext: (child, props) => (
-    <RelayRouterContext {...props}>
-      {child}
-    </RelayRouterContext>
-  ),
+  renderRouterContext: (child, props) => {
+    if (props) {
+      Object.keys(props).forEach(prop => {
+        if (typeof props[prop] === 'function') {
+          props[prop] = props[prop].call(null);
+        }
+      });
+    }
+
+    return (
+      <RelayRouterContext {...props}>
+        {child}
+      </RelayRouterContext>
+    );
+  },
 
   renderRouteComponent: (child, props) => {
     /* eslint-disable react/prop-types */
